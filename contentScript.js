96e0b4b280d4112b48d7719c08a60d1d67a969e6
Kubas445 <jakubsedlak102@gmail.com>
2023-11-12 16:58:27 +0100

    content copy fix

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,9 @@
-let bottomMessage = `<div id="fuck" style="width: 3.5rem;height: 3.5rem;border-radius: 100%;background: white;box-shadow: 0px 0px 10px -1px black;right: 1rem;bottom: 1rem;position: fixed;"><div title='fucked by my extension' style="width: 3.5rem;background:url('${chrome.runtime.getURL(
+/*let bottomMessage = `<div id="fuck" style="width: 3.5rem;height: 3.5rem;border-radius: 100%;background: white;box-shadow: 0px 0px 10px -1px black;right: 1rem;bottom: 1rem;position: fixed;"><div title='fucked by my extension' style="width: 3.5rem;background:url('${
+    chrome.runtime.getURL(
     "icon32.png"
-)}');height: 3.5rem;background-repeat: no-repeat;background-position: center;"></div><p style="position: absolute;top: 50%;right: 100%;width: 15rem;display:none;">Fucked up by my extension</p></div>`;
-document.body.innerHTML += bottomMessage;
+)}');height: 3.5rem;background-repeat: no-repeat;background-position: center;"></div><p style="position: absolute;top: 50%;right: 100%;width: 15rem;display:none;">Fucked up by my extension</p></div>`;*/
+// chrome.runtime.geturl dotn work in MAIN environment
+//document.body.innerHTML += bottomMessage;
 window.onload = function () {
     document.body.parentNode.replaceChild(
         document.body.cloneNode(true),
@@ -12,6 +14,7 @@ window.onload = function () {
         ":not(input):not(textarea), img{user-select:auto!important;}#fuck:hover p {display:block!important}"
     );
     document.body.appendChild(elem);
+
     window.removeEventListener("keyup", window.wpccpDisableKeys);
     window.removeEventListener("keydown", window.wpccpDisableKeys);
     window.removeEventListener("keydown", window.wpccpDisableCtrlActions);
